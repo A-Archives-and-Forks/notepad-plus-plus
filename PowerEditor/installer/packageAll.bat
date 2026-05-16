A-Archives-and-Forks/notepad-plus-plus

commit e49ab3219ed48f1c7cbc61c60c50d031e607821a

Fix MSI installer display random Hexadecimal number as name on UAC
Fix also MiX compiling issue due to system update.

Fix #17967

PowerEditor/installer/packageAll.bat
@@ -23,7 +23,7 @@ REM commands to sign
 
 set signtoolWin11="C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe"
 
-set Sign_by_GlobalSignCert=%signtoolWin11% sign /n "NOTEPAD++" /tr http://timestamp.globalsign.com/tsa/r6advanced1 /td SHA256 /fd SHA256
+set Sign_by_GlobalSignCert=%signtoolWin11% sign /n "NOTEPAD++" /d "Notepad++ Installer" /du "https://notepad-plus-plus.org" /tr http://timestamp.globalsign.com/tsa/r6advanced1 /td SHA256 /fd SHA256
 
 set DOUBLE_SIGNING=/as
 

PowerEditor/installer/msi/nppInstall.wixproj
@@ -11,6 +11,6 @@
 	</PropertyGroup>
 
 	<ItemGroup>
-		<PackageReference Include="WixToolset.Util.wixext" Version="*" />
+		<PackageReference Include="WixToolset.Util.wixext" Version="6.0.2" />
 	</ItemGroup>
 </Project>

commit 14594c83dc743f979d837b28ea9e6b55c391a6db

Fix a UAC display regression on Notepad++ v8.9.5
Regression was introduced by:
https://github.com/notepad-plus-plus/notepad-plus-plus/commit/e49ab3219ed48f1c7cbc61c60c50d031e607821a

Fix https://github.com/notepad-plus-plus/notepad-plus-plus/commit/e49ab3219ed48f1c7cbc61c60c50d031e607821a#r185469222

PowerEditor/installer/packageAll.bat
@@ -23,7 +23,7 @@ REM commands to sign
 
 set signtoolWin11="C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe"
 
-set Sign_by_GlobalSignCert=%signtoolWin11% sign /n "NOTEPAD++" /d "Notepad++ Installer" /du "https://notepad-plus-plus.org" /tr http://timestamp.globalsign.com/tsa/r6advanced1 /td SHA256 /fd SHA256
+set Sign_by_GlobalSignCert=%signtoolWin11% sign /n "NOTEPAD++" /d "Notepad++" /du "https://notepad-plus-plus.org" /tr http://timestamp.globalsign.com/tsa/r6advanced1 /td SHA256 /fd SHA256
 
 set DOUBLE_SIGNING=/as
 